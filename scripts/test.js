f742d5319ae77af9698d300d3523f4328c2f1b90
duc-san <duc.nv194023@sis.hust.edu.vn>
2024-04-09 18:53:24 +0700

    Add project

---
+++ scripts/test.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+const EP_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+async function main() {
+    const code = await hre.ethers.provider.getCode(EP_ADDRESS);
+
+
+    console.log("code to:", code);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+})
